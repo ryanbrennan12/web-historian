7c92745a831a5b068012766b75be3fb84e5df675
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-09-02 14:00:44 -0700

    First Test passing

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,20 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.url === '/' && req.method === 'GET') {
+    fs.readFile(__dirname + '/public/index.html', (err, html) => {
+      // console.log(html, 'this is the chunk');
+      // if (err) {
+      //   throw err;
+      // }
+      res.writeHeader(200, { 'Content-Type': 'text/html' });
+      res.write(html);
+      res.end(html, 'utf-8');
+    });
+  }
+  // res.end(archive.paths.list);
 };
+
