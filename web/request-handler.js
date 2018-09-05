365b4e7b026e5b4ad19b853fff88be306866826a
Ryan Brennan <ryanbrennanbc1@gmail.com>
2018-09-05 15:54:57 -0700

    [WIP] -loading.html running -caching to sites.txt failing now

--- web/request-handler.js
+++ web/request-handler.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
 
-  if (req.method === 'POST') {
+  if (req.method === 'POST' && req.url === '/') {
     var buffer = '';
     req.on('data', (chunk) => {
       // var readStream = fs.createReadStream(__dirname + chunk, 'utf-8');
@@ -22,17 +22,18 @@ exports.handleRequest = function (req, res) {
         if (isArchived) {
       
           //get html body from archived file via fs.readSomething
-          console.log('this is the thing', archive.paths.archivedSites + '/' + exampleUrl);
+          
         } else {
           console.log('hit else statement');
           console.log(archive.paths.siteAssets + '/' + 'loading.html');
-          fs.readFile(archive.paths.loadingPage, (err, data) => {
-            if (err) {
-              throw error;
-            } else {
-              res.writeHeader(200, {'Content-Type': 'text/html'});
-              res.end(body, 'utf-8');
-            }
+          
+          fs.readFile(archive.paths.siteAssets + '/' + 'loading.html', 'utf-8', (err, html) => {
+            // console.log('this is the html', html);
+           
+            console.log('RESSSS', res);
+            res.writeHead(302, {'Content-Type': 'text/html'});
+            res.end(html);
+      
           });
         }
       });
@@ -44,28 +45,21 @@ exports.handleRequest = function (req, res) {
       
     });
     
-  }
-  if (req.url === '/' && req.method === 'GET') {
+  } else if (req.url === '/' && req.method === 'GET') {
     fs.readFile(__dirname + '/public/index.html', (err, html) => {
-      console.log(html, 'this is the chunk');
-      // if (err) {
-      //   throw err;
-      // }
       
-      res.writeHeader(200, { 'Content-Type': 'text/html' });
-      // res.write(html);
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(html, 'utf-8');
     });
   } else {
-    // console.log('this is the DIRRR', req.url);
     fs.readFile(archive.paths.archivedSites + req.url, (err, html) => {
-      console.log('this is the thing', req.url);
+      
       if (err) {
-        res.writeHeader(404, { 'Content-Type': 'text/html' });
+        res.writeHead(404, { 'Content-Type': 'text/html' });
         res.end();
       }
 
-      res.writeHeader(200, { 'Content-Type': 'text/html' });
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(html, 'utf-8');
     });
     
